cbefdfc21444b069be8b9a6ed6935bb344afdf40
Victor Albu <albu.victor@outlook.com>
2017-07-05 10:34:53 +0300

    second line from dev1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world")
+            Console.WriteLine("hello world");
+            Console.WriteLine("second line from dev1");
         }
     }
 }
