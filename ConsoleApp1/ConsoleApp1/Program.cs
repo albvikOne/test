006a3910d7a25116ef7d73771291eca1178f0ca4
Victor Albu <albu.victor@outlook.com>
2017-07-05 10:34:33 +0300

    second line from dev2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -7,7 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world")
+            Console.WriteLine("hello world");
+            Console.WriteLine("second line from dev2");
         }
     }
 }
